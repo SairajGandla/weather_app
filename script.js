6000e30060ca911fb26076fdd1c7d4907cfe792d
SairajGandla <91734931+SairajGandla@users.noreply.github.com>
2024-01-03 15:46:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const dropdown = document.getElementById("dropdown");
 weather.addEventListener("click", () => {
   dropdown.classList.toggle("collapse");
 });
+const timer = setInterval(() => {
+  const date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  document.getElementById("time").innerHTML = `${hours} : ${minutes} ${
+    hours >= 12 ? "PM" : "AM"
+  }`;
+}, 1000);
+
